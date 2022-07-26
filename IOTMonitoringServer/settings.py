7df494922ec0d4728cce9b9ddc991ff045bf5473
coandrade <co.andrade86@uniandes.edu.co>
2022-07-25 20:24:00 -0500

    Update settings.py

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.95.194.89"]
+ALLOWED_HOSTS = ["localhost", "44.203.99.137"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.83.3.57",  # Dirección IP de la base de datos
+        "HOST": "52.54.228.214",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "34.238.247.236"
+MQTT_HOST = "3.85.59.183"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
